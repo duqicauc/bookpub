a79c77c1e511639896d82f17214b5fe128d89720
duqi <duqi@youzan.com>
2017-11-27 21:55:24 +0800

    #Feature Spring Boot中使用拦截器

--- src/main/java/site/javadu/bookpub/WebApplication.java
+++ src/main/java/site/javadu/bookpub/WebApplication.java
@@ -3,15 +3,34 @@ package site.javadu.bookpub;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * 作用: User: duqi Date: 2017/11/26 Time: 19:52
  */
 @Configuration
-public class WebApplication {
+public class WebApplication extends WebMvcConfigurerAdapter {
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+    /**
+     * 显示定义拦截器对应的bean
+     **/
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    /**
+     * 显示添加拦器
+     **/
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
